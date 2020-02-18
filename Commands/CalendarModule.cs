35a4cb6d7da3620560bb27c2227a5c8e093b7dfc
JayJay1989BE <jonas@lateur.pro>
2020-02-18 16:53:19 +0100

    -

--- Commands/CalendarModule.cs
+++ Commands/CalendarModule.cs
@@ -20,27 +20,28 @@ namespace DiscordBot.Commands
             string[] weekdays = {"Zondag", "Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag"};
             List<Dictionary<string, object>> list = new List<Dictionary<string, object>>();
             Dictionary<string, object> newItem = new Dictionary<string, object>();
+            newItem.Add("-", new[]{"VM","NM"});
             for (int i = 1; i <= 5; i++)
             {
                 IEnumerable<TimeTable> thisTimeTable = timeTable.Where(tt => (int) tt.Day.DayOfWeek == i);
                 string today = (i == (int)DateTime.Today.DayOfWeek && weeknumber == null) ? "(*)" : "";
                 if (!thisTimeTable.Any())
                 {
-                    newItem.Add($"{weekdays[i]} {today}", new[] {"Geen Les (VM)", "Geen Les (NM)" });
+                    newItem.Add($"{weekdays[i]} {today}", new[] {"Geen Les", "Geen Les" });
                 }
                 else if (thisTimeTable.Count() == 1)
                 {
                     if(thisTimeTable.First().Time.StartTime.Hour < 13 && thisTimeTable.First().Time.StartTime.Hour > 7)
-                        newItem.Add($"{weekdays[i]} {today}", new[] { $"{thisTimeTable.First().Subject} (VM)", "Geen Les (NM)" });
+                        newItem.Add($"{weekdays[i]} {today}", new[] { $"{thisTimeTable.First().Subject}", "Geen Les" });
                     if(thisTimeTable.First().Time.StartTime.Hour > 12 && thisTimeTable.First().Time.StartTime.Hour < 18)
-                        newItem.Add($"{weekdays[i]} {today}", new[] { "Geen Les (VM)", $"{thisTimeTable.First().Subject} (NM)"});
+                        newItem.Add($"{weekdays[i]} {today}", new[] { "Geen Les", $"{thisTimeTable.First().Subject}"});
                 }
                 else
                 {
                     newItem.Add($"{weekdays[i]} {today}", new[]
                     {
-                        $"{thisTimeTable.First().Subject} (VM)", 
-                        $"{thisTimeTable.Last().Subject} (NM)"
+                        $"{thisTimeTable.First().Subject}", 
+                        $"{thisTimeTable.Last().Subject}"
                     });
                 }
                 
